3e44dd2a752796b4352c830e74e36616beece6f4
Tal <talsmd95@gmail.com>
2025-05-17 02:33:29 +0300

    Add API for the donations with wrangler(CluodFlare) and D1(Database)

---
+++ donation-api/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE donations (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  owner TEXT NOT NULL,
+  text TEXT NOT NULL,
+  uploadDate TEXT NOT NULL,
+  itemsNeeded TEXT NOT NULL,
+  costRange TEXT NOT NULL,
+  currentDonations TEXT NOT NULL,
+  donationOptions TEXT NOT NULL,
+  donationLimit TEXT NOT NULL
+);
